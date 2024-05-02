34241bbaf1eb8d4598be80d0272e93d762177531
Barry Digby <Barry.Digby@pfizer.com>
2024-05-02 18:08:41 +0100

    foo

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -2,14 +2,13 @@ library(shinytest2)
 
 test_that("{shinytest2} recording: branch_protection", {
   app <- AppDriver$new(variant = platform_variant(), name = "branch_protection", 
-      height = 577, width = 979)
-  app$set_inputs(clusters = 4)
-  app$set_inputs(clusters = 3)
+      seed = 123, height = 577, width = 979)
   app$set_inputs(clusters = 2)
   app$expect_screenshot()
   app$set_inputs(clusters = 3)
   app$expect_screenshot()
   app$set_inputs(clusters = 4)
   app$expect_screenshot()
+  app$expect_values()
 })
 
